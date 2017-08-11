85c9c8ccc511351f7cf2860497564d9fbcf7ea36
WillTamagi <will.tamagi@gmail.com>
2017-08-11 19:53:03 -0300

    Added component Header.

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link'
+import styled from 'styled-components'
+
+const Header = styled.header`
+  position: relative;
+  padding: 3rem 10%;
+  background-color: #29292f;
+  color: white;
+`
+export default Header
