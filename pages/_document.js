b78ee95c758c3c7c3d4c8a3ee83511fa29d8798c
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2017-08-25 15:19:11 -0300

    Added base.css and fonts.

--- pages/_document.js
+++ pages/_document.js
@@ -1,28 +1,28 @@
+import React from 'react'
 import Document, { Head, Main, NextScript } from 'next/document'
-import styled, { ServerStyleSheet } from 'styled-components'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
   render () {
     const sheet = new ServerStyleSheet()
     const main = sheet.collectStyles(<Main />)
     const styleTags = sheet.getStyleElement()
+
     return (
       <html>
         <Head>
           <title>My page</title>
-          {styleTags}
+          <link href='/static/base.css' rel='stylesheet' />
+          <link href='https://fonts.googleapis.com/css?family=Alegreya:400italic,700italic,400,700' rel='stylesheet' type='text/css' />
+          { styleTags }
         </Head>
-        <Body>
+        <body>
           <div>
             {main}
           </div>
           <NextScript />
-        </Body>
+        </body>
       </html>
     )
   }
 }
-
-const Body = styled.body`
-  margin: 0;
-`
